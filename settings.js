96c7cc68ca78186dc004ba65d381c94d82b01e81
Kyle Ibo <kooldude.gamedev@gmail.com>
2026-03-14 14:07:05 +0800

    Updated destination URL

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = {
   projectName: "Cebuano AI Speech-to-Text Demo",
   stableUrl: "https://kooldudegamedev.github.io/qr-display/",
-  destinationUrl: "https://alien-abc-independence-burke.trycloudflare.com",
+  destinationUrl: "https://english-author-usgs-theater.trycloudflare.com",
   autoRedirectSeconds: 5,
   qrImagePath: "./assets/qr-code.png",
   boothTitle: "Scan to Try the Demo",
