c9cc4f4aa69d93af8e5cffa0a3a688f7b5d89c89
Kyle Ibo <kooldude.gamedev@gmail.com>
2026-03-16 09:26:32 +0800

    Updated destination URL

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = {
   projectName: "Cebuano AI Speech-to-Text Demo",
   stableUrl: "https://kooldudegamedev.github.io/qr-display/",
-  destinationUrl: "https://populations-heart-leisure-wage.trycloudflare.com",
+  destinationUrl: "https://template-january-trunk-royal.trycloudflare.com",
   autoRedirectSeconds: 5,
   qrImagePath: "./assets/qr-code.png",
   boothTitle: "Scan to Try the Demo",
